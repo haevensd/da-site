18f307905e7f66ec05ce71d28c890f7bac95e405
Haeven Kelley <haevenshane@gmail.com>
2025-01-03 20:35:03 -0600

    change chevron

--- src/app/access/page.js
+++ src/app/access/page.js
@@ -127,9 +127,9 @@ const CoursePlayer = () => {
 
   return (
     <div className="course-player-container">
-      {!mobileSidebarClosed && <img className="arrow" onClick={() => setMobileSidebarClosed(!mobileSidebarClosed)} style={{position:'absolute', top: '30px',zIndex: 1000, left: '20px', marginBottom: '1rem', transform: 'rotate(180deg)'}} width={15} height={20} src="/chevron-left.png"></img>}
+      {!mobileSidebarClosed && <img className="arrow" onClick={() => setMobileSidebarClosed(!mobileSidebarClosed)} style={{position:'absolute', top: '30px',zIndex: 1000, left: '20px', marginBottom: '1rem', transform: 'rotate(180deg)'}} width={10} height={20} src="/chevron-left.png"></img>}
       <div className={`course-sidebar ${!mobileSidebarClosed && 'is-mobile'}`}>
-      {mobileSidebarClosed && <img className="arrow" onClick={() => setMobileSidebarClosed(!mobileSidebarClosed)} style={{position:'absolute', top: '20px', right: '20px', marginBottom: '1rem', marginLeft: 'auto'}} width={15} height={20} src="/chevron-left.png"></img>}
+      {mobileSidebarClosed && <img className="arrow" onClick={() => setMobileSidebarClosed(!mobileSidebarClosed)} style={{position:'absolute', top: '20px', right: '20px', marginBottom: '1rem', marginLeft: 'auto'}} width={10} height={20} src="/chevron-left.png"></img>}
         {courseModules.map((module, moduleIndex) => (
           <div key={moduleIndex} className="module-section">
             <div onClick={() => toggleSection(moduleIndex)} style={{backgroundColor: '#f79c26', padding: '5px', paddingBottom: '1px', borderTopLeftRadius: '5px', borderTopRightRadius: '5px', borderBottomLeftRadius: '2px', borderBottomRightRadius: '2px'}}>
