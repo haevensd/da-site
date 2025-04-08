72cc78c1f499007f9f53b3b22ef1ade34935a768
haevensd <haevendickerson@gmail.com>
2025-04-07 20:56:08 -0500

    ew

--- src/app/course/page.js
+++ src/app/course/page.js
@@ -17,7 +17,7 @@ const FreeCourseForm = () => {
   });
 
   return (
-    <div className="vault-container" style={{padding: '0', paddingBottom:'0'}}>
+    <div className="vault-container" style={{padding: '0', paddingBottom:'0', minHeight: '24800px'}}>
       <div style={{display: 'flex', background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(22,60,156,1) 0%, rgba(38,107,248,1) 30%, rgb(10, 199, 236) 95%)', color: 'white'}} className="hero-container" >
         <div style={{margin: 'auto', maxWidth: '',paddingTop: '4rem', }}>
           <h2 style={{color: 'orange', fontWeight: 'bold', fontSize: '42px',maxWidth: '1170px',textAlign: 'center',color: '#ffbb2a',marginTop: '20px', marginBottom: '5px', marginLeft: 'auto', marginRight: 'auto',fontFamily: 'Gotham Book Bold'}}> I Built a Business From Home That Generated Millions With Teams of Tens of Thousands—Now I’m Giving You the Full Program Free</h2>
@@ -1692,7 +1692,7 @@ const FreeCourseForm = () => {
 
       <hr style={{ boxShadow: '0 2px 5px 2px rgba(0,0,0,0.4)', marginTop: '50px', backgroundColor: 'white', color: 'white' }} />
 
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', }}>
+      <div style={{ display: 'flex', justifyContent: 'center',paddingBottom:'525px', alignItems: 'center', flexDirection: 'column', }}>
         <h4 style={{ color: 'rgb(7,91,183)', fontSize: '24px', fontWeight: '700', marginTop: '50px', }}>SO, JUST TO SUM IT ALL UP...</h4>
         <p style={{ marginBottom: '40px', fontSize: '24px' }}>Here's What's You're About To Get: </p>
         <hr style={{ marginBottom: '30px', color: 'inherit', width: '100%' }} />
