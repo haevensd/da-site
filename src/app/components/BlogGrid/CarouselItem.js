bbc671fe778d9563461790cd5f759f72e292e212
haevensd <haevendickerson@gmail.com>
2025-06-25 15:39:10 -0500

    fonts

--- src/app/components/BlogGrid/CarouselItem.js
+++ src/app/components/BlogGrid/CarouselItem.js
@@ -8,8 +8,8 @@ export default function CarouselItem({ imgUrl, imgTitle,id, link ,text}) {
     <div className="carousel-card" style={{position: 'relative', overflow: 'hidden'}}>
       
       <div src={'/play-icon.png'} id="play-icon" style={{position: 'absolute', width: '90%',padding: '0px',paddingTop:'0',top: '20px', height: '70px', overflow:'ellipses', whiteSpace: '', textOverflow: 'ellipsis'}} >
-      <h1 style={{fontSize: '24px',color: 'white', minWidth: '100%' }}>{imgTitle}</h1>
-      <p style={{color:'white', maxWidth: '80%'}}>{text}</p>
+      <h1 style={{fontFamily: 'Gotham Book Bold',fontSize: '24px',color: 'white', minWidth: '100%' }}>{imgTitle}</h1>
+      <p style={{fontFamily: 'Gotham Book',color:'white', maxWidth: '80%'}}>{text}</p>
       </div>
       <img src={imgUrl} alt={imgTitle}></img>
     </div>
